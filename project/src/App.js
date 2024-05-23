d99bcfe6237cdaa294e9e0728f12a21f7640ab9d
PC <muoivo.051064@gmail.com>
2024-05-23 18:46:27 +0700

    update tailwind lib

--- project/src/App.js
+++ project/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
-
+import branch from './assets/logo/branch.svg'
 function App() {
   return (
     <div className="App">
-      Router in here
+       <h1 className="text-5xl font-bold underline">
+      Hello world!
+    </h1>
     </div>
   );
 }
