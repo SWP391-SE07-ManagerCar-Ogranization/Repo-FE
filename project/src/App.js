04fd2e8ca04829ab471b39755f137bd64c8ede3b
PC <muoivo.051064@gmail.com>
2024-05-24 19:15:32 +0700

    update lib form and alert

--- project/src/App.js
+++ project/src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
-import branch from './assets/logo/branch.svg'
+import logo from './assets/icon/logo.svg'
+import React, { useState } from 'react';
 function App() {
+  const [inputValue, setInputValue] = useState('');
+
+  // Hàm xử lý sự kiện onBlurs
+  const handleBlur = (event) => {
+    console.log('Input lost focus');
+    // Thực hiện các thao tác xử lý khi phần tử mất focus, ví dụ: kiểm tra dữ liệu, xác thực, lưu trữ, vv.
+  };
+
   return (
-    <div className="App">
-       <h1 className="text-5xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+      <h1>hello</h1>
+     
     </div>
   );
 }
