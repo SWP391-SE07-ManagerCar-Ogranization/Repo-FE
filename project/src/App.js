261ed8e175b2a8afe25260084ad02f336b9cfdec
PC <muoivo.051064@gmail.com>
2024-05-27 19:13:59 +0700

    update login page UI 27/5

--- project/src/App.js
+++ project/src/App.js
@@ -1,7 +1,6 @@
 import 'react-toastify/dist/ReactToastify.css';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import LoginPage from './component/auth/LoginPage';
-import LoginPage2 from './component/auth/LoginPage2';
 import HomePage from './component/home/HomePage';
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
@@ -12,7 +11,6 @@ function App() {
               <GoogleOAuthProvider clientId="966533859873-a39qra6o0qhqjcp6jt2sbg9f2csqlssg.apps.googleusercontent.com">
             <Routes>
             <Route path="/login" element={<LoginPage/>}></Route>
-            <Route path="/login2" element={<LoginPage2/>}></Route>
             <Route path="/" element={<HomePage/>}></Route>
             </Routes>
             </GoogleOAuthProvider>
--- project/src/component/auth/LoginPage.jsx
+++ project/src/component/auth/LoginPage.jsx
@@ -1,41 +1,43 @@
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import * as UserService from "../../service/UserService";
+import { useFormik } from "formik";
+import { signupValidation } from "../../config/signupValidation";
 import { GoogleLogin } from "@react-oauth/google";
 import { jwtDecode } from "jwt-decode";
+import * as UserService from "../../service/UserService";
+import { Link, useNavigate } from "react-router-dom";
+import logo from "../../assets/icon/logo.svg";
 
-function LoginPage() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+function LoginPage2() {
   const [error, setError] = useState("");
   const navigate = useNavigate();
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const userData = await UserService.login(email, password);
+  const initValues = {
+    email: "",
+    password: "",
+  };
+
+  const { values, handleBlur, handleChange, handleSubmit, errors } = useFormik({
+    initialValues: initValues,
+    validationSchema: signupValidation,
+    onSubmit: async (values) => {
+      const userData = await UserService.login(values.email, values.password);
       console.log(userData);
       if (userData.token) {
         localStorage.setItem("token", userData.token);
         localStorage.setItem("role", userData.role);
-        navigate("/profile");
+        navigate("/");
       } else {
-        setError(userData.message);
+        setError("Invalid email or password");
       }
-    } catch (error) {
-      console.log(error);
-      setError(error.message);
-      setTimeout(() => {
-        setError("");
-      }, 5000);
-    }
-  };
+    },
+  });
+
   const handleSubmitGoogle = async (formData) => {
     try {
-        const userData = await UserService.loginGoogle(formData);
+      const userData = await UserService.loginGoogle(formData);
       if (userData.token) {
         localStorage.setItem("token", userData.token);
         localStorage.setItem("role", userData.role);
-        navigate("/profile");
+        navigate("/");
       } else {
         setError(userData.message);
       }
@@ -48,48 +50,121 @@ function LoginPage() {
     }
   };
 
+  const onSuccessGoogle = async (credentialResponse) => {
+    const credentialResponseDecoded = jwtDecode(credentialResponse.credential);
+    const newFormData = {
+      name: credentialResponseDecoded.name,
+      email: credentialResponseDecoded.email,
+    };
+    handleSubmitGoogle(newFormData);
+  };
 
   return (
-    <div className="auth-container">
-      <h2>Login</h2>
-      <div>
-      <GoogleLogin
-        onSuccess={(credentialResponse) => {
-            const credentialResponseDecoded = jwtDecode(credentialResponse.credential);
-            const newFormData = {
-                name: credentialResponseDecoded.name,
-                email: credentialResponseDecoded.email
-              };
-              handleSubmitGoogle(newFormData);
-        }}
-        onError={() => {
-          console.log("Login Failed");
-        }}
-        
-      />  
+    <section className="bg-gradient-to-r from-red-50 via-red-200 to-red-400 h-screen">
+      <div className="px-0 py-10 mx-auto max-w-7xl sm:px-4">
+        <div className="sm:w-10/12 md:w-8/12 lg:w-6/12 xl:w-4/12 sm:px-6 text-gray-400">
+          <img src={logo} alt="logo" className="mx-auto"/>
+        </div>
+        <div className="w-full px-4 pt-5 pb-6 mx-0 mt-8 mb-6 rounded-none shadow-xl sm:rounded-lg sm:w-10/12 md:w-8/12 lg:w-6/12 xl:w-4/12 sm:px-6">
+          <h1 className="mb-4 text-lg font-semibold text-center text-gray-600">
+            Login into your account
+          </h1>
+
+          <form className="mb-8 space-y-4" onSubmit={handleSubmit}>
+            <label
+              htmlFor="email"
+              className="mb-1 text-xs font-medium text-gray-700"
+            >
+              Email address
+            </label>
+            <input
+              type="text"
+              id="email"
+              name="email"
+              autoComplete="email"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              value={values.email}
+              onBlur={handleBlur}
+              onChange={handleChange}
+            />
+            {errors.email && (
+              <small className="mt-2 text-sm text-red-600 dark:text-red-500">
+                {errors.email}
+              </small>
+            )}
+            <br></br>
+            <label
+              htmlFor="password"
+              className="mb-1 text-xs font-medium text-gray-700"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              autoComplete="current-password"
+              value={values.password}
+              onBlur={handleBlur}
+              onChange={handleChange}
+            />
+            {errors.password && <small>{errors.password}</small>}
+            {error && (
+              <p className="mt-2 text-sm text-red-600 dark:text-red-500 text-center font-semibold">
+                {error}
+              </p>
+            )}
+            <button
+              className="text-gray-900 bg-gradient-to-r from-red-200 via-red-300 to-yellow-200 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 w-full text-white"
+              type="submit"
+            >
+              Login
+            </button>
+          </form>
+
+          <div className="space-y-8">
+            <div
+              className="text-center border-b border-gray-400"
+              style={{ lineHeight: "0px" }}
+            >
+              <span
+                className="p-2 text-xs font-semibold tracking-wide text-orange-600 uppercase bg-red-100 rounded-lg"
+                style={{ lineHeight: "0px" }}
+              >
+                Or
+              </span>
+            </div>
+            <div className="grid gap-4 justify-items-center">
+              <GoogleLogin
+                onSuccess={onSuccessGoogle}
+                onError={() => {
+                  console.log("Login Failed");
+                }}
+                useOneTap
+                shape="pill"
+              />
+            </div>
+          </div>
+        </div>
+        <p className="sm:w-10/12 md:w-8/12 lg:w-6/12 xl:w-4/12 sm:px-6 text-gray-400 flex justify-between">
+          <Link
+            to={"/"}
+            className="text-orange-500 hover:underline dark:text-primary-500 font-semibold "
+          >
+            Create an account
+          </Link>
+          -
+          <Link
+            to={"/"}
+            className="text-orange-500 hover:underline dark:text-primary-500 font-semibold "
+          >
+            Forgot password ?
+          </Link>
+        </p>
       </div>
-      {error && <p className="error-message">{error}</p>}
-      <form onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label>Email: </label>
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
-        <div className="form-group">
-          <label>Password: </label>
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <button type="submit">Login</button>
-      </form>
-    </div>
+    </section>
   );
 }
 
-export default LoginPage;
+export default LoginPage2;
