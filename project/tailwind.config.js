06141927d1d7dd820aa061b3d10512a40de54d71
Nguyen Duc Thinh <thinhnnguyenndt248@gmail.com>
2024-06-27 16:06:34 +0700

    PlacesMap-ducthinh 27.06.2024

--- project/tailwind.config.js
+++ project/tailwind.config.js
@@ -4,7 +4,11 @@ const withMT = require("@material-tailwind/react/utils/withMT");
 module.exports = withMT({
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage:{
+        'parallax': 'url("D:/fpt/SWP/fcar/front-end/project/src/assets/images/car1.png")'
+      }
+    },
   },
   plugins: [],
 })
